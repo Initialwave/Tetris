4e912b01c2f3625f62e6938532ed02859c6ab17b
Daniel Ratzlaff <69171385+Initialwave@users.noreply.github.com>
2021-11-20 17:12:20 -0600

    styled, but errors in the way I'm calling my props, so not rendering properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Tetris from './components/Tetris';
+import Tetris from "./components/Tetris";
 
-let App = () => (
-  <div classname="App">
+const App = () => (
+  <div className="App">
     <Tetris />
   </div>
-)
+);
 
 export default App;
